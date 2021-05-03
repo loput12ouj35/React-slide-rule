52ddbe9b60faae90ff50c95b1042318a51b59417
jaeyong_park <jaeyong_park@tmax.co.kr>
2021-05-04 00:54:36 +0900

    feat(app): Add examples
    - Add examples related to width

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
 import SliderRuler from 'SliderRuler';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 export default React.memo(function App() {
-  const root = useRef();
-  const test = useRef();
   const [value, setValue] = useState(150);
 
+  return (
+    <>
+      <Width100PercentExample />
+      <ResizeObserverExample />
+      <SliderRuler value={value} onChange={setValue} gap={5} />
+      <SliderRuler value={value} onChange={setValue} gap={20} />
+      <SliderRuler value={value} onChange={setValue} precision={2} />
+      <SliderRuler gap={20} precision={0.01} />
+    </>
+  );
+});
+
+const Width100PercentExample = React.memo(function () {
+  const [value, setValue] = useState(150);
+  const [ref, setRef] = useState(null);
+  const width = ref?.offsetWidth;
+
+  return (
+    <div ref={(node) => setRef(node)}>
+      <SliderRuler value={value} onChange={setValue} width={width} />
+    </div>
+  );
+});
+
+const ResizeObserverExample = React.memo(function () {
+  const [value, setValue] = useState(150);
+  const root = useRef();
+  const [width, setWidth] = useState(300);
+  const adjustWidth = useCallback(() => setWidth(root.current.offsetWidth), []);
+
   useEffect(() => {
-    const observer = new ResizeObserver(test.current.adjustSize);
+    const observer = new ResizeObserver(adjustWidth);
     observer.observe(root.current);
     return () => observer.unobserve();
   }, []);
 
   return (
     <div ref={root}>
-      <SliderRuler ref={test} value={value} onChange={setValue} />
-      <SliderRuler ref={test} value={value} onChange={setValue} divide={5} />
-      <SliderRuler ref={test} value={value} onChange={setValue} divide={20} />
-      <SliderRuler ref={test} value={value} onChange={setValue} precision={2} />
-      <SliderRuler divide={20} precision={0.01} />
+      <SliderRuler value={value} onChange={setValue} width={width} />
     </div>
   );
 });

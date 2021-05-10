36565fa2d0f4641730e001b75b521efe0b7c7507
JY PARK <jaeyong_park@tmax.co.kr>
2021-05-10 12:47:46 +0900

    maintain(app): Update webpack.config
    - Use 'config-name' to determine dev/production mode, instead of 'mode'.
    - Add webpack.config.js to .eslintignore.
    - Remove unused items in .gitIgnore.

--- webpack.config.js
+++ webpack.config.js
@@ -2,59 +2,42 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  mode: isProduction ? 'production' : 'development',
-  devtool: isProduction ? 'hidden-source-map' : 'eval',
-
+const base = {
   entry: ['@babel/polyfill', './src/index.jsx'],
-
   resolve: {
-    modules: ['node_modules', path.resolve(__dirname, 'src')],
+    modules: ['node_modules', 'src'],
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
-
   module: {
     rules: [
       {
-        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         use: ['babel-loader', 'ts-loader', 'eslint-loader'],
         exclude: /node_modules/,
       },
     ],
   },
-
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-
   plugins: [
-    new webpack.ProvidePlugin({
-      React: 'react',
-    }),
-    new HtmlWebpackPlugin(
-      Object.assign(
-        { template: './src/index.html' },
-        isProduction
-          ? {
-              minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeRedundantAttributes: true,
-                useShortDoctype: true,
-                removeEmptyAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                keepClosingSlash: true,
-                minifyJS: true,
-                minifyCSS: true,
-                minifyURLs: true,
-              },
-            }
-          : undefined
-      )
-    ),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
     new webpack.HotModuleReplacementPlugin(),
   ],
 };
+
+module.exports = [
+  {
+    name: 'dev',
+    mode: 'development',
+    devtool: 'eval',
+    ...base,
+  },
+  {
+    name: 'prod',
+    mode: 'production',
+    devtool: 'hidden-source-map',
+    ...base,
+  },
+];

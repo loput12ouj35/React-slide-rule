ec776f1dc675fb216e34142fcd5ffd84a7417da8
jaeyong_park <jaeyong_park@tmax.co.kr>
2021-05-11 00:26:03 +0900

    maintain(app): Remove typescript-related configs
    - Remove the configs of example app, as it is no longer use typescript.

--- example-demo-app/webpack.config.js
+++ example-demo-app/webpack.config.js
@@ -6,13 +6,13 @@ const base = {
   entry: ['@babel/polyfill', './src/index.jsx'],
   resolve: {
     modules: ['node_modules', 'src'],
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx|ts|tsx)$/,
-        use: ['babel-loader', 'ts-loader', 'eslint-loader'],
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader', 'eslint-loader'],
         exclude: /node_modules/,
       },
     ],

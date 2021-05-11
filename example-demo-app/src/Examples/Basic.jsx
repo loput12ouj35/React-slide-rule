6fdbe5cb0e347b77662f713bd2952046910f3fb5
JY PARK <loput12ouj35@gmail.com>
2021-05-11 21:58:47 +0900

    feat(app): Update component version
    - Update to 1.0.1
    - Simplify the basic example

--- example-demo-app/src/Examples/Basic.jsx
+++ example-demo-app/src/Examples/Basic.jsx
@@ -1,20 +1,17 @@
 import React, { useState } from 'react';
 import SlideRule from 'react-slide-rule';
 
-const STYLES = {
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    position: 'relative',
-  },
+const STYLE = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
 };
 
 export default React.memo(function () {
   const [value, setValue] = useState(150);
 
   return (
-    <div style={STYLES.root}>
+    <div style={STYLE}>
       <p>{value}</p>
       <SlideRule value={value} onChange={setValue} />
     </div>

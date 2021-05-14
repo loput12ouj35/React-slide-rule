5434abe5d63b85cd80803b8d8b67df7677b4e051
JY PARK <loput12ouj35@gmail.com>
2021-05-14 19:08:21 +0900

    temp(app): Add links to examples
    - Add link to source codes for the examples.

--- example-demo-app/src/App.jsx
+++ example-demo-app/src/App.jsx
@@ -48,12 +48,14 @@ export default React.memo(function App() {
         <ExampleItem
           id="full-width"
           title="Full width"
+          subheader="By using the ref of a wrapper, you can stretch the component to its width. Notice that the component will not resize."
           fileName="FullWidth"
           example={<FullWidthExample />}
         />
         <ExampleItem
           id="resize-observer"
           title="Full width + ResizeObserver"
+          subheader="By using ResizeObserver, the component "
           fileName="ResizeObserver"
           example={<ResizeObserverExample />}
         />

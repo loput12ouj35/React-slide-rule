b40261c37626524e658fbcd778186b5f02253d77
JY PARK <loput12ouj35@gmail.com>
2021-05-14 19:08:21 +0900

    temp(app): Add links to examples
    - Add link to source codes for the examples.

--- example-demo-app/src/ExampleItem.jsx
+++ example-demo-app/src/ExampleItem.jsx
@@ -12,6 +12,7 @@ export default React.memo(function ExampleItem(props) {
       {title && <h3 id={id}>{title}</h3>}
       <p>{subheader}</p>
       {example}
+      <a href={`${prefixPath + fileName}.jsx`}>&#x1F517; View code</a>
     </li>
   );
 }, isAlwaysEqual);

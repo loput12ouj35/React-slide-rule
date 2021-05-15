e948fa6e74c444e89b22f8dc8208bd5cc4244658
JY PARK <loput12ouj35@gmail.com>
2021-05-15 15:58:22 +0900

    feat(app): Add descriptions
    - Add descriptions of the examples.
    - Split some props of ExampleItem into two files: description.js and exampleMap.jsx

--- example-demo-app/src/App.jsx
+++ example-demo-app/src/App.jsx
@@ -1,64 +1,29 @@
 import React from 'react';
-import {
-  InputNumberExample,
-  FullWidthExample,
-  ResizeObserverExample,
-  BasicExample,
-  VerticalExample,
-  CustomStyledExample,
-  CustomCursorExample,
-} from 'Examples';
 import ExampleItem from 'ExampleItem';
+import descriptions from 'Data/descriptions';
 
 export default React.memo(function App() {
   return (
     <main>
       <h1>Demos</h1>
+      <p>
+        <a href="https://github.com/loput12ouj35/React-slide-rule">
+          React-slide-rule
+        </a>
+        {descriptions.main}
+      </p>
       <h2 id="basic">Basic</h2>
       <ul>
-        <ExampleItem fileName="Basic" example={<BasicExample />} />
-        <ExampleItem
-          id="input-element"
-          title="With an input element"
-          fileName="InputNumber"
-          example={<InputNumberExample />}
-        />
-        <ExampleItem
-          id="vertical"
-          title="Vertical example"
-          fileName="Vertical"
-          example={<VerticalExample />}
-        />
+        <ExampleItem id="basic" />
+        <ExampleItem id="input-element" />
+        <ExampleItem id="vertical" />
       </ul>
       <h2 id="Advanced">Advanced</h2>
       <ul>
-        <ExampleItem
-          id="custom-styled"
-          title="Custom Styled"
-          fileName="CustomStyled"
-          example={<CustomStyledExample />}
-        />
-        <ExampleItem
-          id="custom-cursor"
-          title="Custom cursor"
-          subheader="Numbers above marks"
-          fileName="CustomCursor"
-          example={<CustomCursorExample />}
-        />
-        <ExampleItem
-          id="full-width"
-          title="Full width"
-          subheader="By using the ref of a wrapper, you can stretch the component to its width. Notice that the component will not resize."
-          fileName="FullWidth"
-          example={<FullWidthExample />}
-        />
-        <ExampleItem
-          id="resize-observer"
-          title="Full width + ResizeObserver"
-          subheader="By using ResizeObserver, the component "
-          fileName="ResizeObserver"
-          example={<ResizeObserverExample />}
-        />
+        <ExampleItem id="custom-styled" />
+        <ExampleItem id="custom-cursor" />
+        <ExampleItem id="full-width" />
+        <ExampleItem id="resize-observer" />
       </ul>
       <h2>Playgournd</h2>
       <p>(todo)</p>
--- example-demo-app/src/ExampleItem.jsx
+++ example-demo-app/src/ExampleItem.jsx
@@ -1,18 +1,17 @@
+import descriptions from 'Data/descriptions';
+import exampleMap from 'Data/exampleMap';
 import React from 'react';
 
-const prefixPath =
-  'https://github.com/loput12ouj35/React-slide-rule/blob/main/example-demo-app/src/Examples/';
-const isAlwaysEqual = () => true;
-
-export default React.memo(function ExampleItem(props) {
-  const { id, title, subheader, fileName, example } = props;
+export default React.memo(function ExampleItem({ id }) {
+  const { title, subheader } = descriptions[id];
+  const { fileName, example } = exampleMap[id];
 
   return (
     <li>
       {title && <h3 id={id}>{title}</h3>}
       <p>{subheader}</p>
       {example}
-      <a href={`${prefixPath + fileName}.jsx`}>&#x1F517; View code</a>
+      <a href={fileName}>&#x1F517; View code</a>
     </li>
   );
-}, isAlwaysEqual);
+});
